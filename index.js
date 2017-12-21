197c32581b83706837987000a4b0185f9fd74ad5
Edward Norton <edwardnorton@icloud.com>
2017-12-21 02:23:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string) }
-console.log()
+function sayHiToGrandma(string) {
+  console.log(string.toLowerCase())
+  if 
+}
