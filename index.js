24be943e7907987ba9f380cff2d13f9bdeb4d32c
Edward Norton <edwardnorton@icloud.com>
2017-12-21 02:48:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,16 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase
-}
+  if (string === string.toLowerCase()) {
+    var lowerCase = string.toLowerCase();
+    return ('I can\'t hear you!');
+     }
+     else if (string === string.toUpperCase()){
+       var upperCase = 'YES INDEED!';
+       return ('YES INDEED!');
+       }
+       else{
+         var mixedCase = 'I love you, too.';
+         return(mixedCase);
+       }
+     }
